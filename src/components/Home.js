3eda222baa71493079980c2b9a50497f78a1020a
L Graham <lgraham92@gmail.com>
2020-09-16 16:32:04 -0600

    Changed Homepage, altered CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,14 @@ function HomePage() {
     return (
         <div id='homeDiv'>
             <h2> Lindseys Basic Calculator </h2>
-            <h3> This Project Uses: React, Redux, React-Redux, React-Router-Dom</h3>
+            <h3> This Project Uses the Following Frameworks:</h3>
+            <ul>
+                <li><a href='https://en.wikipedia.org/wiki/HTML5' target='_blank'><img src={require("../image/html5.png")} alt='HTML5 Logo'></img></a></li>
+                <li><a href='https://en.wikipedia.org/wiki/CSS' target='_blank'><img src={require("../image/css.png")} alt='CSS Logo'></img></a></li>
+                <li><a href='https://reactjs.org/' target='_blank'><img src={require("../image/react.png")} alt='React Logo'></img></a></li>
+                <li><a href='https://redux.js.org/' target='_blank'><img src={require("../image/redux.png")} alt='Redux Logo'></img></a></li>
+                <li><a href='https://react-redux.js.org/' target='_blank'><img src={require("../image/react-redux.png")} alt='React-Redux Logo'></img></a></li>
+            </ul>
         </div>
     );
 }
